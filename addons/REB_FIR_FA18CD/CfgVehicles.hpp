0c882f411074c11bd6675c53ea14b008966781a7
Mingi Park <mgkid3310@naver.com>
2021-08-17 12:07:17 +0900

    initial class add

---
+++ addons/REB_FIR_FA18CD/CfgVehicles.hpp
@@ -0,0 +1,6 @@
+class CfgVehicles {
+	class Plane_Fighter_03_base_F;
+
+	class FIR_FA18_Base: Plane_Fighter_03_base_F {};
+	class FIR_FA18D_Base: Plane_Fighter_03_base_F {};
+};
---
+++ addons/REB_FIR_FA18CD/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+	class REB_FIR_FA18CD {
+		name = "REBalanced FIR F/A-18CD";
+		author = "Orbis2358";
+		requiredVersion = 2.00;
+		requiredAddons[] = {"orbis_aerodynamics", "FIR_FA18_F", "FIR_FA18D_F"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "CfgVehicles.hpp"
