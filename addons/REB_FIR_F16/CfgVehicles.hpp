c5fb28e2e30402e925177a7822a1af3970047a2a
Orbis2358 <mgkid3310@naver.com>
2021-09-27 16:12:18 +0900

    add engine config on variations

--- addons/REB_FIR_F16/CfgVehicles.hpp
+++ addons/REB_FIR_F16/CfgVehicles.hpp
@@ -117,6 +117,114 @@ class CfgVehicles {
 			externalTank[] = {"FIR_F16C_Fueltank_P_1rnd_M", "1120 / 3200", "FIR_F16C_center_Fueltank_P_1rnd_M", "910 / 3200"};
 		};
 	};
+	class FIR_F16C: FIR_F16_Base { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_Standard: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_AV: FIR_F16C { // Blk 40 (510th & 555th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16C_ROKAF: FIR_F16C { // Blk 52
+		#include "config\F100-PW-229.hpp"
+	};
+	class FIR_F16C_ROKAF_B32: FIR_F16C { // Blk 32
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_ROKAF_Hess: FIR_F16C { // Blk 52
+		#include "config\F100-PW-229.hpp"
+	};
+	class FIR_F16C_WW: FIR_F16C { // Blk 50 (13th FS)
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_Polish: FIR_F16C { // Blk 52
+		#include "config\F100-PW-229.hpp"
+	};
+	class FIR_F16C_HG: FIR_F16C { // Blk 42 (122th FS)
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_SW_HGV: FIR_F16C { // Blk 50 (77th FS)
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_WP: FIR_F16C { // Blk 40 (35th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16C_WP_Juvat: FIR_F16C { // Blk 40 (80th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16C_Osan: FIR_F16C { // Blk 40 (36th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16C_Makos: FIR_F16C { // Blk 30 (93rd FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16C_HO_8FS: FIR_F16C { // Blk 40 (8th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16C_HO_311FS: FIR_F16C { // Blk 42 (311th FS)
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_HO_49WG: FIR_F16C { // Blk 42, not certain (49th WG)
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_Blank: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_Blank2: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_Blank3: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_MERC: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_Ag_arctic: FIR_F16C { // PW-220
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_Ag_flanker: FIR_F16C { // PW-220
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_Ag_lizard: FIR_F16C { // PW-220
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F16C_ENEMY_TYPE_A: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_CFTTEST: FIR_F16C { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16C_Polish_CFT : FIR_F16C_CFTTEST { // Blk 52+
+		#include "config\F100-PW-229.hpp"
+	};
 
 	class FIR_F16D_Base: Plane_Fighter_03_base_F {};
+	/* class FIR_F16D: FIR_F16D_Base { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16D_WP: FIR_F16D { // Blk 40 (80th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16D_WP_Fanton: FIR_F16D { // Blk 40 (35th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16D_Blank: FIR_F16D { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16D_Standard: FIR_F16D { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16D_AV: FIR_F16D { // Blk 40 (510th & 555th FS)
+		#include "config\F110-GE-100.hpp"
+	};
+	class FIR_F16D_ROKAF: FIR_F16D { // Blk 52
+		#include "config\F100-PW-229.hpp"
+	};
+	class FIR_F16D_CFTTEST: FIR_F16D { // not defined
+		#include "config\F110-GE-129.hpp"
+	};
+	class FIR_F16D_Polish_CFT: FIR_F16D { // Blk 52+
+		#include "config\F100-PW-229.hpp"
+	}; */
 };
