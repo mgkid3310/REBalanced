52d44475526aff365ec141e55f0070ea952ac210
Mingi Park <mgkid3310@naver.com>
2023-06-07 18:47:43 +0900

    make code simpler

--- addons/REB_FIR_F16/config.cpp
+++ addons/REB_FIR_F16/config.cpp
@@ -3,7 +3,7 @@ class CfgPatches {
 		name = "REBalanced FIR F-16";
 		author = "Orbis2358";
 		requiredVersion = 2.04;
-		requiredAddons[] = {"orbis_aerodynamics", "FIR_F16_F", "FIR_F16D_F", "REB_FIR_F16_preConfig"};
+		requiredAddons[] = {"orbis_aerodynamics", "REB_FIR_F16_preConfig"};
 		units[] = {};
 		weapons[] = {};
 	};
