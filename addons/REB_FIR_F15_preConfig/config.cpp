8b9c537ad8e8f7101008dbef1cee1aef37d9b504
Mingi Park <mgkid3310@naver.com>
2023-06-07 18:48:26 +0900

    add F15 preconfig

---
+++ addons/REB_FIR_F15_preConfig/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+	class REB_FIR_F15_preConfig {
+		name = "REBalanced FIR F-15 PreConfig";
+		author = "Orbis2358";
+		requiredVersion = 2.04;
+		requiredAddons[] = {"orbis_aerodynamics", "FIR_F15_F", "FIR_F15_Legacy_F", "FIR_F15EX_F", "FIR_F15SE_F"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "CfgVehicles.hpp"
