9c40e1b719f5fa51e95d436cd21bb09c8ef2080a
Mingi Park <mgkid3310@naver.com>
2023-06-08 16:25:41 +0900

    fix macro

--- addons/REB_FIR_F15/script_component.hpp
+++ addons/REB_FIR_F15/script_component.hpp
@@ -1,9 +1,14 @@
 #define ADDON REB_FIR_F15
 
-#define QUOTE(var) "var"
-#define QPATH(var) QUOTE(ADDON/var)
+#define DOUBLES(var1,var2) var1##_##var2
+#define TRIPLES(var1,var2,var3) var1##_##var2##_##var3
+#define QUOTE(var1) #var1
 
-#define QPATHUI(var) QUOTE(ADDON/ui/var)
-#define GVAR(var) ADDON_var
-#define FUNC(var) ADDON_fnc_var
-#define PREP(var) FUNC(var) = compile preProcessFileLineNumbers QPATH(functions/var.sqf)
+#define QPATH(var1) QUOTE(ADDON\var1)
+
+#define QPATHUI(var1) QUOTE(ADDON\ui\var1)
+#define QPATHFUNC(var1) QUOTE(ADDON\functions\var1)
+
+#define GVAR(var1) DROUBLES(ADDON,var1)
+#define FUNC(var1) TRIPLES(ADDON,fnc,var1)
+#define PREP(var1) FUNC(var1) = compile preProcessFileLineNumbers QPATHFUNC(var1.sqf)
