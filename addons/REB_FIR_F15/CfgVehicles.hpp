2b3db4c0795b98339c72759704175a2bae30d81e
Mingi Park <mgkid3310@naver.com>
2023-06-09 11:24:23 +0000

    apply config to variants

--- addons/REB_FIR_F15/CfgVehicles.hpp
+++ addons/REB_FIR_F15/CfgVehicles.hpp
@@ -6,7 +6,39 @@ class CfgVehicles {
 		#include "config\common.hpp"
 		#include "config\F100-PW-220.hpp"
 	};
-	class FIR_F15C: FIR_F15_Base {
+	class FIR_F15D_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
 		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F15E_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-229.hpp"
+	};
+
+	class FIR_F15A_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F15B_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-220.hpp"
+	};
+	class FIR_F15E_Demo_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-229.hpp"
+	};
+	class FIR_F15B_Civ_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-220.hpp"
+	};
+
+	class FIR_F15EX_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-229.hpp"
+	};
+
+	class FIR_F15SE_Base: Plane_Fighter_03_base_F {
+		#include "config\common.hpp"
+		#include "config\F100-PW-229.hpp"
 	};
 };
