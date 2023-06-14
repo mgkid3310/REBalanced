be18b2d06b6f0e07f7405d6f5c4e02e63e14e48c
Mingi Park <mgkid3310@naver.com>
2023-06-14 19:57:58 +0900

    improve preconfig

--- addons/REB_FIR_FA18CD_preConfig/CfgVehicles.hpp
+++ addons/REB_FIR_FA18CD_preConfig/CfgVehicles.hpp
@@ -1,22 +1,10 @@
 class CfgVehicles {
 	class Plane_Fighter_03_base_F;
 	class FIR_FA18_Base: Plane_Fighter_03_base_F {
-		delete draconicTorqueXCoef;
-		delete draconicTorqueYCoef;
-
-		class UserActions {
-			delete Afterburner;
-			delete Afterburner_1;
-		};
+		#include "CfgDelete.hpp"
 	};
 
 	class FIR_FA18D_Base: Plane_Fighter_03_base_F {
-		delete draconicTorqueXCoef;
-		delete draconicTorqueYCoef;
-
-		class UserActions {
-			delete Afterburner;
-			delete Afterburner_1;
-		};
+		#include "CfgDelete.hpp"
 	};
 };
