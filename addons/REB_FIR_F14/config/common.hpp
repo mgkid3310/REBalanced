0f78fe5a4892fa779d358a419a7a1d63f1876c73
Mingi Park <mgkid3310@naver.com>
2023-06-26 03:00:34 +0900

    modify control performance

--- addons/REB_FIR_F14/config/common.hpp
+++ addons/REB_FIR_F14/config/common.hpp
@@ -17,14 +17,14 @@
 		elevatorControlsSensitivityCoef = 4.0;
 		aileronControlsSensitivityCoef = 4.0;
 		rudderControlsSensitivityCoef = 2.0;
-		wheelSteeringSensitivity = 6.0;
-		elevatorSensitivity = 0.800;
-		aileronSensitivity = 0.980;
-		rudderInfluence = 0.344;
+		wheelSteeringSensitivity = 1.4;
+		elevatorSensitivity = 0.560;
+		aileronSensitivity = 0.680;
+		rudderInfluence = 0.360;
 
 		airFrictionCoefs0[] = {0, 0, 0};
-		airFrictionCoefs1[] = {0.01, 0.02, 0.008};
-		airFrictionCoefs2[] = {0.001, 0.002, 0.00001};
+		airFrictionCoefs1[] = {0.01, 0.04, 0.008};
+		airFrictionCoefs2[] = {0.001, 0.004, 0.00001};
 		draconicForceXCoef = 7.0;
 		draconicForceYCoef = 1.1;
 		draconicForceZCoef = 1.0;
@@ -33,9 +33,9 @@
 
 		angleOfIndicence = 0.0523599; // 3deg
 		envelope[] = {0, 0.3, 0.9, 1.8, 2.6, 3.2, 3.6, 3.8, 3.9, 3.7, 3.4, 3, 2.7, 2.5, 2.45, 2.4, 2.4, 2.4, 2.4, 2.4, 2.4};
-		elevatorCoef[] = {1, 1.6, 1.8, 1.7, 1.55, 1.4, 1.2, 1, 0.8, 0.65, 0.55, 0.5, 0.48, 0.46, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45};
-		aileronCoef[] = {1, 1.6, 1.8, 1.7, 1.55, 1.4, 1.2, 1, 0.8, 0.65, 0.55, 0.5, 0.48, 0.46, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45};
-		rudderCoef[] = {1, 1.6, 1.8, 1.7, 1.55, 1.4, 1.2, 1, 0.8, 0.65, 0.55, 0.5, 0.48, 0.46, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45};
+		aileronCoef[] = {1, 1.6, 1.8, 1.88, 1.9, 1.85, 1.75, 1.6, 1.4, 1.2, 1.05, 0.92, 0.85, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8, 0.8};
+		elevatorCoef[] = {1, 1.55, 1.7, 1.65, 1.55, 1.4, 1.2, 1, 0.8, 0.65, 0.55, 0.5, 0.48, 0.46, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45};
+		rudderCoef[] = {1, 1.55, 1.7, 1.65, 1.55, 1.4, 1.2, 1, 0.8, 0.65, 0.55, 0.5, 0.48, 0.46, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45, 0.45};
 
 		unitInfoType = "Rsc_REB_F14_UnitInfo";
 
